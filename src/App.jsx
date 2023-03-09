b045b44595978d51e3849ce6948409b6947c3c77
marina <nicolaimarina@gmail.com>
2023-03-09 17:43:31 -0500

    edits to deplay

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
@@ -8,6 +7,10 @@ function App() {
   return (
     <div className="App">
         <h1>app</h1>
+        <p>nav</p>
+        <p>herosection</p>
+        <p>aboutsection</p>
+        <p>skillssection</p>
     </div>
   )
 }
